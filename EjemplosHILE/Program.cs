7ebb604a2dda032a5c409421c43fcf8aed61213d
kamiarnold73 <kamilla_arnold24@unikino.edu.mx>
2024-10-21 07:44:51 -0700

    Agregar archivos de proyecto.

---
+++ EjemplosHILE/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EjemplosHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int repetir = 100;
+            int i = 0; //Este es un contador
+
+            while (i < repetir) 
+            {
+                i ++;
+                Console.WriteLine("Bienvenido i=" + i);
+            }
+
+            
+        }
+    }
+}
